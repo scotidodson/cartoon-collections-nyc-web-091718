58d28520a63120b317bac82d68eb60d94f068c25
Scoti Dodson <scotidodson@gmail.com>
2018-08-29 19:38:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
-seven_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "five", "six", "seven"]
+
 
 def roll_call_dwarves(dwarves)
+  seven_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "five", "six", "seven"]
   numbered_dwarves = []
   seven_dwarves.each_with_index do |name, index|
     number = index + 1 
