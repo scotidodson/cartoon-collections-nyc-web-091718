60df6f071bec0999884f27e2b3f25ae335088463
Scoti Dodson <scotidodson@gmail.com>
2018-08-29 19:44:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
   puts numbered_dwarves
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(# code an argument here)
   # Your code here
 end
 
