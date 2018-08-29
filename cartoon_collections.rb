926fcbd34f525536a6bd1fb8228e5f184486e873
Scoti Dodson <scotidodson@gmail.com>
2018-08-29 19:53:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(food)
-  food.map! { |item| item.capitalize + "1" }
+  food.map! { |item| item.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
